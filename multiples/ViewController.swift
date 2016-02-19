83b9d5de770f7f2dc153674a71b2cc9128e37e25
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-02-19 23:37:19 +0000

    finished project

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // outlets
     @IBOutlet weak var logo: UIImageView!
     @IBOutlet weak var inputField: UITextField!
     @IBOutlet weak var playBtn: UIButton!
@@ -17,12 +18,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLbl: UILabel!
     @IBOutlet weak var addBtn: UIButton!
     
+    
+    // variables
+    var numberMultiplied = 0
+    var numberOne = 0
+    var resultNumber = 0
+    
+    
+    //functions
+    func add() -> Int {
+        resultNumber = numberOne + numberMultiplied
+        return resultNumber
+    }
+    
+    
+    
+    //code
  
     @IBAction func startPlay(sender: AnyObject) {
+        logo.hidden = true
+        inputField.hidden = true
+        playBtn.hidden = true
+        
+        resultLbl.hidden = false
+        addBtn.hidden = false
+        
+        numberMultiplied = Int(inputField.text!)!
     }
 
     @IBAction func addNumber(sender: AnyObject) {
+        
+        add()
+        
+        resultLbl.text = "\(numberOne) + \(numberMultiplied) = \(resultNumber)"
+        
+        numberOne = resultNumber
+        
     }
 
 }
-
